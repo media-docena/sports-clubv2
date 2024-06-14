dfd3cefbba3c45ccdf09ced65e531233df896c70
unknown <josefinacicchini@gmail.com>
2024-06-14 00:31:09 -0300

    Add carnet de socio form

---
+++ clubdeportivoV2/frmCarnet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clubdeportivoV2
+{
+    public partial class frmCarnet : Form
+    {
+        public frmCarnet()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImprimirCarnet_Click(object sender, EventArgs e)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.PrintPage += new PrintPageEventHandler(printCarnet_PrintPage);
+            pd.Print();
+            btnImprimirCarnet.Visible = true; // visualizamos nuevamente el objeto.
+
+            MessageBox.Show("Operación existosa", "AVISO DEL SISTEMA",
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+            frmPrincipal principal = new frmPrincipal();
+            principal.Show();
+            this.Close();
+           
+        }
+
+        private void printCarnet_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            int x = SystemInformation.WorkingArea.X;
+            int y = SystemInformation.WorkingArea.Y;
+            int ancho = this.Width;
+            int alto = this.Height;
+            Rectangle bounds = new Rectangle(x, y, ancho, alto);
+            Bitmap img = new Bitmap(ancho, alto);
+            this.DrawToBitmap(img, bounds);
+            Point p = new Point(100, 100);
+            e.Graphics.DrawImage(img, p);
+        }
+    }
+}
